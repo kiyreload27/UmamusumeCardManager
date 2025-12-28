72dfc34893de847a4bb98137885f7b5443a92a61
kiyreload27 <xtrickline@gmail.com>
2025-12-28 17:10:23 +0000

    Removal OF files

--- updater/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Updater module for UmamusumeCardManager
-Handles checking for updates and downloading new versions from GitHub Releases.
-"""
-
-from updater.update_checker import check_for_updates, download_update, apply_update
-
-__all__ = ['check_for_updates', 'download_update', 'apply_update']
